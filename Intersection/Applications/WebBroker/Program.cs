20e9a2776d1ef95cae70782ad4ac213bf25c5b66
ObeA <obe@obeattema.nl>
2019-03-03 17:54:23 +0100

    WIP

---
+++ Intersection/Applications/WebBroker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebBroker
+{
+    public class Program
+    {
+        public static async Task Main(string[] args) => await BuildWebHost(args).RunAsync();
+
+        private static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
