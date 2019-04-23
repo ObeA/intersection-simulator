5f52afd5b9add7512c604842db86972435b456f5
ObeA <obe@obeattema.nl>
2019-04-23 12:30:30 +0200

    ...

--- Intersection Simulation/Assets/Scripts/VehicleBrain.cs
+++ Intersection Simulation/Assets/Scripts/VehicleBrain.cs
@@ -1,4 +1,5 @@
-﻿using Intersection;
+﻿using System;
+using Intersection;
 using PathCreation;
 using UnityEngine;
 
@@ -19,7 +20,7 @@ public class VehicleBrain : MonoBehaviour
             return; // TODO: remove this hack
         }
 
-        if (HandleInDirection(transform.forward))
+        if (HandleInDirection(transform.forward, lookAhead))
         {
             return;
         }
@@ -29,34 +30,36 @@ public class VehicleBrain : MonoBehaviour
         var curvatureVector = (nextPoint - currentPoint).normalized;
         var dot = Vector3.Dot(curvatureVector, transform.forward);
         
-        if (HandleInDirection(Quaternion.Euler(0, 10, 0) * transform.forward) || HandleInDirection(curvatureVector))
+        if (HandleInDirection(Quaternion.Euler(0, 20, 0) * curvatureVector, lookAhead * 0.75f) 
+            || HandleInDirection(curvatureVector, lookAhead))
         {
             return;
         }
         
         Debug.DrawRay(currentPoint, transform.forward * lookAhead * 1.5f, Color.white);
-        Debug.DrawRay(currentPoint, Quaternion.Euler(0, 10, 0) * transform.forward * lookAhead * 1.5f, Color.cyan);
+        Debug.DrawRay(currentPoint, Quaternion.Euler(0, 20, 0) * curvatureVector * lookAhead * 0.75f, Color.cyan);
         Debug.DrawRay(currentPoint, curvatureVector * lookAhead * 1.5f, Color.blue);
         
-        Vehicle.ChangeSpeed(Vehicle.topSpeed * dot, Vehicle.topSpeed);
+        Vehicle.ChangeSpeed(Vehicle.topSpeed * dot * dot, Vehicle.topSpeed);
         _state = VehicleState.Cruising;
     }
 
-    private bool HandleInDirection(Vector3 direction)
+    private bool HandleInDirection(Vector3 direction, float distance)
     {
-        if (Physics.Raycast(transform.position, direction, out var hitInfo, lookAhead * 1.5f, -5, QueryTriggerInteraction.Ignore))
+        
+        if (Physics.Raycast(transform.position, direction, out var hitInfo, distance * 1.5f, -5, QueryTriggerInteraction.Ignore))
         {
             Debug.DrawRay(transform.position, direction * hitInfo.distance, Color.yellow);
 
             var otherFollower = hitInfo.collider.gameObject.GetComponent<PathFollower>();
             if (otherFollower != null)
             {
-                HandleHitWithOtherVehicle(otherFollower, hitInfo);
+                HandleHitWithOtherVehicle(otherFollower, hitInfo, distance);
                 return true;
             }
 
             var trafficLight = hitInfo.collider.gameObject.GetComponentInParent<TrafficLight>();
-            if (trafficLight != null && HandleCollisionWithTrafficLight(trafficLight, hitInfo))
+            if (trafficLight != null && HandleCollisionWithTrafficLight(trafficLight, hitInfo, distance))
             {
                 return true;
             }
@@ -71,9 +74,9 @@ public class VehicleBrain : MonoBehaviour
         _state = VehicleState.Stopping;
     }
 
-    private void HandleHitWithOtherVehicle(PathFollower follower, RaycastHit hitInfo)
+    private void HandleHitWithOtherVehicle(PathFollower follower, RaycastHit hitInfo, float distance)
     {
-        if (hitInfo.distance < lookAhead * 0.5f && (follower.Acceleration <= 0 || Mathf.Approximately(follower.CurrentSpeed, 0)))
+        if (hitInfo.distance < distance * 0.5f && (follower.Acceleration <= 0 || Mathf.Approximately(follower.CurrentSpeed, 0)))
         {
             Vehicle.ForceSpeed(follower.CurrentSpeed);
         }
@@ -81,7 +84,7 @@ public class VehicleBrain : MonoBehaviour
         {
             Vehicle.ChangeSpeed(follower.CurrentSpeed, -Vehicle.CurrentSpeed / (hitInfo.distance / Vehicle.CurrentSpeed));
         }
-        else if (hitInfo.distance > lookAhead)
+        else if (hitInfo.distance > distance)
         {
             Vehicle.ChangeSpeed(Mathf.Min(follower.CurrentSpeed, Vehicle.topSpeed), Mathf.Min(follower.Acceleration, Vehicle.topSpeed));
         }
@@ -97,7 +100,7 @@ public class VehicleBrain : MonoBehaviour
         _state = VehicleState.Tailing;
     }
 
-    private bool HandleCollisionWithTrafficLight(TrafficLight trafficLight, RaycastHit hitInfo)
+    private bool HandleCollisionWithTrafficLight(TrafficLight trafficLight, RaycastHit hitInfo, float distance)
     {
         switch (trafficLight.State)
         {
@@ -105,13 +108,13 @@ public class VehicleBrain : MonoBehaviour
                 return false;
             case TrafficLight.TrafficLightState.Red:
             case TrafficLight.TrafficLightState.Transitioning when (Time.time - trafficLight.LastStateChange) > 2:
-                if (hitInfo.distance < lookAhead * 0.5f)
+                if (hitInfo.distance < distance * 0.5f)
                 {
                     Vehicle.ForceSpeed(0);
                 }
                 else
                 {
-                    Vehicle.ChangeSpeed(0, -Vehicle.CurrentSpeed / (Mathf.Max(hitInfo.distance, lookAhead) / Vehicle.CurrentSpeed));
+                    Vehicle.ChangeSpeed(0, -Vehicle.CurrentSpeed / (Mathf.Max(hitInfo.distance, distance) / Vehicle.CurrentSpeed));
                 }
 
                 _state = VehicleState.Waiting;
